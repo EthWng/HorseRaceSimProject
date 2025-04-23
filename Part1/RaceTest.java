c0b5b5bb198a7b9ef65d2ab17eecef39295f1223
Ethan Wong <ec24490@qmul.ac.uk>
2025-04-23 20:22:58 +0100

    changed lane numbers to test if the program could handle it

--- Part1/RaceTest.java
+++ Part1/RaceTest.java
@@ -11,7 +11,7 @@ public class RaceTest {
     //testing the race using with 3 horses
     public static void testNormalRace(int distance) {
         System.out.println("=== Test: Normal Race ===");
-        Race race = new Race(distance);
+        Race race = new Race(distance, 5);
         Horse h1 = new Horse('A', "Name1", 1);
         Horse h2 = new Horse('B', "Name2", 0.85);
         Horse h3 = new Horse('C', "Name3", 0.7);
@@ -26,7 +26,7 @@ public class RaceTest {
     //testing if it works if missing 1 horse at end
     public static void testMissingHorse1(int distance) {
         System.out.println("=== Test: Missing Race ===");
-        Race race = new Race(distance);
+        Race race = new Race(distance, 2);
         Horse h1 = new Horse('A', "Name1", 0.9);
         Horse h2 = new Horse('B', "Name2", 0.85);
 
@@ -39,7 +39,7 @@ public class RaceTest {
     //testing if it works if missing 1 horse at start
     public static void testMissingHorse2(int distance) {
         System.out.println("=== Test: Missing Race ===");
-        Race race = new Race(distance);
+        Race race = new Race(distance, 1);
         Horse h2 = new Horse('A', "Name1", 0.9);
         Horse h3 = new Horse('C', "Name3", 0.7);
 
@@ -52,7 +52,7 @@ public class RaceTest {
     //testing if it works if theres only 1 horse 
     public static void test1Horse(int distance) {
         System.out.println("=== Test: Missing2 Race ===");
-        Race race = new Race(distance);
+        Race race = new Race(distance, 4);
         Horse h1 = new Horse('A', "Name1", 0.9);
 
         race.addHorse(h1, 1);
