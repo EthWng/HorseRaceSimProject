89438472636ea97feb05c62f9b34f2912ded05ae
Ethan Wong <ec24490@qmul.ac.uk>
2025-04-23 16:27:26 +0100

    testing the race with missing horses

--- Part1/RaceTest.java
+++ Part1/RaceTest.java
@@ -3,6 +3,8 @@ public class RaceTest {
         int distance = 10;
         testNormalRace(distance);
         System.out.println("");
+        testMissingHorse1(distance);
+        testMissingHorse2(distance);
     }
 
     //testing the race using with 3 horses
@@ -18,5 +20,31 @@ public class RaceTest {
         race.addHorse(h3, 3);
 
         race.startRace();  // should show a proper race run
+    }
+
+    //testing if it works if missing 1 horse at end
+    public static void testMissingHorse1(int distance) {
+        System.out.println("=== Test: Missing Race ===");
+        Race race = new Race(distance);
+        Horse h1 = new Horse('A', "Name1", 0.9);
+        Horse h2 = new Horse('B', "Name2", 0.85);
+
+        race.addHorse(h1, 1);
+        race.addHorse(h2, 2);
+
+        race.startRace();  
+    }
+
+    //testing if it works if missing 1 horse in middle
+    public static void testMissingHorse2(int distance) {
+        System.out.println("=== Test: Missing Race ===");
+        Race race = new Race(distance);
+        Horse h2 = new Horse('A', "Name1", 0.9);
+        Horse h3 = new Horse('C', "Name3", 0.7);
+
+        race.addHorse(h2, 2);
+        race.addHorse(h3, 3);
+
+        race.startRace();
     }
 }
